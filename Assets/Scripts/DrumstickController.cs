13d7adeb7d685cc41722d87da36682de92dec76a
Hanousi <tawil.hani@gmail.com>
2018-12-01 17:58:14 +0000

    First commit: Drumkit, avatar, grabbable drumsticks and they make sound!

---
+++ Assets/Scripts/DrumstickController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class DrumstickController : MonoBehaviour {
+
+    // Use this for initialization
+    void Start () {
+  
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+        AudioSource audioData = other.GetComponent<AudioSource>();
+        audioData.Play(0);
+        Debug.Log("started");
+    }
+}
