5d279e7230dc43a95645142802c0eacc87872ead
Hanousi <tawil.hani@gmail.com>
2019-02-03 18:29:05 +0000

    Updated version of oculus integration package. Spawned in a menu which displays a button which can be clicked. Fixed collider for ride cymbal due to accidental collisions when trying to hit the high tom

---
+++ Assets/MainScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+// Show off all the Debug UI components.
+public class MainScript : MonoBehaviour
+{
+    bool inMenu;
+    private Text sliderText;
+
+    void Start()
+    {
+        DebugUIBuilder.instance.AddDivider();
+        DebugUIBuilder.instance.AddButton("Start Game", StartGame);
+        DebugUIBuilder.instance.AddDivider();
+
+        inMenu = false;
+    }
+
+    void Update()
+    {
+        if (OVRInput.GetDown(OVRInput.Button.Two) || OVRInput.GetDown(OVRInput.Button.Start))
+        {
+            if (inMenu) DebugUIBuilder.instance.Hide();
+            else DebugUIBuilder.instance.Show();
+            inMenu = !inMenu;
+        }
+    }
+
+    void StartGame()
+    {
+        Debug.Log("Button pressed");
+    }
+}
